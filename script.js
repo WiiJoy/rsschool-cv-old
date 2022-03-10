ef65786c949893198d8fb2c6558afd32c2a2e334
WiiJoy MacOS <wiijoy@bk.ru>
2022-03-10 08:28:32 +0300

    feat: add display descr with mouseover

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const examplies = document.querySelectorAll('.examplies__edu')
+    const examplies = document.querySelector('.examplies__edu')
+
+    examplies.addEventListener('mouseover', (ev) => {
+        console.log(ev.target)
+        if (!ev.target.classList.contains('examplies__item__img')) return
+
+        // console.log('examplies__item')
+
+        let exampleItem = ev.target.nextElementSibling
+
+        console.log(exampleItem)
+
+        exampleItem.style.display = 'block'
+        setTimeout(() => {
+            exampleItem.style.opacity = 1
+        }, 100)
+    })
 })
